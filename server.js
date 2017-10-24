2d6f50e2a70140636011671f62e7516e2c429c89
Cristin <cristin_baciu@yahoo.com>
2017-10-24 20:12:18 +0300

    Added mongoose

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
 
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/my_db');
+
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use('/css',express.static(__dirname + '/css'));
 app.use('/js',express.static(__dirname + '/js'));
 app.use('/assets',express.static(__dirname + '/assets'));
@@ -15,3 +27,14 @@ server.listen(process.env.PORT || 8081,function(){
     console.log('Listening on '+server.address().port);
 });
 
+app.post('/login_page', function (req, res) {
+	console.log(req.body);
+	
+  var post = req.body
+  
+  if (post.user === 'asd' && post.password === 'asd') {
+    res.sendFile(__dirname+'/login_page.html');
+  } else {
+    res.send('Bad user/pass');
+  }
+});
